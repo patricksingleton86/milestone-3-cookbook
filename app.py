89fa34081a12cdb4330b8a167025e7258ee0cf17
patricksingleton86 <patricksingleton95@gmail.com>
2021-02-10 17:05:37 +0000

    Added add_recipe template

--- app.py
+++ app.py
@@ -98,6 +98,11 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
