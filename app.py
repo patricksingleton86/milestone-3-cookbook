54a485c74b7201166cf2e6119ab9ec3cae530a2e
patricksingleton86 <patricksingleton95@gmail.com>
2021-02-12 17:18:30 +0000

    Added functions to add_recipe form

--- app.py
+++ app.py
@@ -99,8 +99,22 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_recipe")
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_description": request.form.get("recipe_description"),
+            "ingredients": request.form.get("ingredients").split(','),
+            "preparation": request.form.get("preparation"),
+            "preparation_time": request.form.get("preparation_time"),
+            "created_by": session["user"]
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe Added Successfully")
+        return redirect(url_for("get_recipes"))
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recipe.html", categories=categories)
 
